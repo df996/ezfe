88b798d9c420c65659e2f017fa3739360cbd4b2a
feng1313 <1236192@qq.com>
2020-09-23 14:46:57 +0800

    +1

---
+++ examples/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>1</div>
+</template>
+
+<script>
+import message from '@ezfe/message'
+console.log (message)
+export default {
+  name: 'APP'
+}
+</script>
---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  pages: {
+    index: {
+      entry: 'examples/main.js',
+      template: 'examples/index.html',
+      filename: 'index.html',
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    }
+  },
+
+  devServer: {
+    port: 8899
+  }
+}
