fe59891a579e56420a964d5ac0e243077493f8bc
大风 <7972918+dafeng1@user.noreply.gitee.com>
2020-11-24 11:27:04 +0800

    :fire:

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,26 @@
 module.exports = {
   entry: 'i2222ndex.html',
   port: 8899,
-  resolvers: [{
-    alias (id) {
-      console.log (id, arguments)
+  //resolvers: [{
+  //  alias (id) {
+  //    console.log (id, arguments)
+  //  }
+  //}]
+  resolvers: [
+    {
+      requestToFile (publicPath, root) {
+        console.log ('-Request to file-----------------------------------------')
+        console.log (publicPath, root)
+        console.log ('---------------------------------------------------------')
+      },
+
+      alias (id) {
+        console.log ('-Alias---------------------------------------------------')
+        console.log (id)
+        console.log ('---------------------------------------------------------')
+      }
     }
-  }]
+  ]
 }
 
 
